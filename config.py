dd070ca18544e268fbe1a070bdf2880c7894ac2f
Kevin Zita <bleedgreenandgold@gmail.com>
2016-02-28 22:55:17 -0500

    Finish db intro section

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Required for Flask-SQLAlchemy (it's the path to our db_file)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# Folder where we store the migration data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
